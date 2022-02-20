f236a7de41befd434ab21e16c37be2e221bf4644
AC <37120385+anthonychg@users.noreply.github.com>
2022-02-19 18:07:36 -0800

    Create LogicalProcessingOrder.sql

---
+++ LogicalProcessingOrder.sql
@@ -0,0 +1,19 @@
+--https://blog.sqlauthority.com/2020/11/18/sql-server-logical-processing-order-of-the-select-statement/
+
+
+/*
+Here is the list of all the logical processing orders.
+
+FROM
+ON
+JOIN
+WHERE
+GROUP BY
+WITH CUBE or WITH ROLLUP
+HAVING
+SELECT
+DISTINCT
+ORDER BY
+TOP
+
+*/
